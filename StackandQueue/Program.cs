b805267e510c9ceb7c0fa6799881074c3ba5f4f1
PriyankaRumal <priyankarumal182000@gmail.com>
2022-11-11 23:32:56 -0800

    solution added

--- StackandQueue/Program.cs
+++ StackandQueue/Program.cs
@@ -25,6 +25,12 @@ namespace StackandQueue
             queue.Enqueue(30);
             queue.Enqueue(70);
             queue.display();
+            queue.Dequeue();
+            queue.display();
+            queue.Dequeue();
+            queue.display();
+            queue.Dequeue();
+            queue.display(); ;
         }
     }
 }
